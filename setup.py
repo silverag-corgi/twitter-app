e4dec4baa613a5756c804c52f6f500c0b4c18241
Rem_ <black.white.red.are.the.best@gmail.com>
2022-02-07 01:06:20 +0900

    [c_add] ライブラリのセットアップ管理ファイルを変更

--- setup.py
+++ setup.py
@@ -1,20 +1,3 @@
-from setuptools import setup, find_packages
-import twitter_lib_for_me
+from setuptools import setup
 
-
-setup(
-    name='twitter-lib-for-me',
-    version=twitter_lib_for_me.__version__,
-    # description='',
-    # long_description='',
-    # url='',
-    # author='',
-    # author_email='',
-    # license='',
-    # classifiers='',
-    # keywords='',
-    # project_urls={},
-    packages=find_packages(),
-    install_requires=['tweepy~=4.4.0'],
-    python_requires='>=3.10',
-)
+setup()
