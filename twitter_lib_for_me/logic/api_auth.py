53dd79ae0003830610e6166d29d8429383b8d402
Rem_ <black.white.red.are.the.best@gmail.com>
2022-02-08 00:48:44 +0900

    [c_mod] 型チェックによるリファクタリングを実施
    VSCodeの拡張機能であるPylanceの設定を変更
    変更前："python.analysis.typeCheckingMode": "off"
    変更後："python.analysis.typeCheckingMode": "basic"

--- twitter_lib_for_me/logic/api_auth.py
+++ twitter_lib_for_me/logic/api_auth.py
@@ -1,7 +1,7 @@
 import io
 import json
 from logging import Logger
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 import python_lib_for_me
 import tweepy
@@ -9,55 +9,59 @@ import tweepy
 
 def do_logic() -> tweepy.API:
     '''ロジック実行'''
-
+    
     try:
         json_data: Dict[Any, Any] = __get_api_auth_info_json()
         api: tweepy.API = __exec_twitter_auth(json_data)
     except Exception as e:
         raise(e)
-
+        
     return api
 
 
 def __get_api_auth_info_json() -> Dict[Any, Any]:
     '''API認証情報JSON取得'''
-
+    
+    lg: Optional[Logger] = None
+    
     try:
-        lg: Logger = python_lib_for_me.get_logger(__name__)
-
+        lg = python_lib_for_me.get_logger(__name__)
+        
         json_file_obj: io.TextIOWrapper = open('config/api_auth_info.json', 'r')
         json_data_dct: Dict[Any, Any] = json.load(json_file_obj)
-
+        
         lg.info(f'API認証情報JSON取得に成功しました。')
     except Exception as e:
         if lg is not None:
             lg.info(f'API認証情報JSON取得に失敗しました。')
         raise(e)
-
+    
     return json_data_dct
 
 
 def __exec_twitter_auth(json_data_dct: Dict[Any, Any]) -> tweepy.API:
     '''Twitter認証実行'''
-
+    
+    lg: Optional[Logger] = None
+    
     try:
-        lg: Logger = python_lib_for_me.get_logger(__name__)
-
+        lg = python_lib_for_me.get_logger(__name__)
+        
         consumer_key: str = json_data_dct['twitter_auth']['consumer_key']
         consumer_secret: str = json_data_dct['twitter_auth']['consumer_secret']
         access_token: str = json_data_dct['twitter_auth']['access_token']
         access_token_secret: str = json_data_dct['twitter_auth']['access_token_secret']
-
+        
         auth: tweepy.OAuthHandler = tweepy.OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_token, access_token_secret)
-
+        
         api: tweepy.API = tweepy.API(auth)
         api.verify_credentials()
-
+        
         lg.info(f'Twitter認証実行に成功しました。')
     except Exception as e:
         if lg is not None:
             lg.info(f'Twitter認証実行に失敗しました。')
         raise(e)
-
+    
     return api
