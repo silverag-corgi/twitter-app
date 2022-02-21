e86426a10ef5305006b740867846d58b76adee8b
Rem_ <black.white.red.are.the.best@gmail.com>
2022-02-22 02:14:01 +0900

    [c_add] レート制限表示を実装

---
+++ twitter_lib_for_me/main/rate_limit_show.py
@@ -0,0 +1,106 @@
+import argparse
+import os
+import sys
+from logging import Logger
+from typing import Optional
+
+import python_lib_for_me as mylib
+import tweepy
+from twitter_lib_for_me.logic import api_auth
+from twitter_lib_for_me.util.twitter_api_standard_v1_1 import \
+    twitter_developer_util
+
+
+def main() -> int:
+    
+    '''
+    メイン
+    
+    Summary:
+        コマンドラインから実行する。
+        
+        引数を検証して問題ない場合、レート制限を表示する。
+    
+    Args:
+        -
+    
+    Args on cmd line:
+        resource_family (str)   : [必須] リソース群
+        endpoint (str)          : [必須] エンドポイント
+    
+    Returns:
+        int: 終了コード(0：正常、1：異常)
+    '''
+    
+    lg: Optional[Logger] = None
+    
+    try:
+        # ロガー取得
+        lg = mylib.get_logger(__name__)
+        
+        # 実行コマンド表示
+        sys.argv[0] = os.path.basename(sys.argv[0])
+        lg.info(f'実行コマンド：{sys.argv}')
+        
+        # 引数取得＆検証
+        args: argparse.Namespace = __get_args()
+        if __validate_args(args) == False:
+            return 1
+        
+        # Twitter認証ロジックの実行
+        api: tweepy.API = api_auth.do_logic()
+        
+        # レート制限の表示
+        twitter_developer_util.show_rate_limit(api, args.resource_family, args.endpoint)
+    except Exception as e:
+        if lg is not None:
+            lg.exception('', exc_info=True)
+        return 1
+    
+    return 0
+
+
+def __get_args() -> argparse.Namespace:
+    '''引数取得'''
+    
+    try:
+        parser: argparse.ArgumentParser = argparse.ArgumentParser(
+                formatter_class=argparse.RawTextHelpFormatter,
+                exit_on_error=True
+            )
+        
+        help_msg: str = ''
+        
+        # 必須の引数
+        help_msg =  'リソース群\n' + \
+                    '例：application'
+        parser.add_argument('resource_family', help=help_msg)
+        help_msg =  'エンドポイント\n' + \
+                    '例：/application/rate_limit_status'
+        parser.add_argument('endpoint', help=help_msg)
+        
+        args: argparse.Namespace = parser.parse_args()
+    except Exception as e:
+        raise(e)
+    
+    return args
+
+
+def __validate_args(args: argparse.Namespace) -> bool:
+    '''引数検証'''
+    
+    lg: Optional[Logger] = None
+    
+    try:
+        # ロガー取得
+        lg = mylib.get_logger(__name__)
+        
+        # 検証：なし
+    except Exception as e:
+        raise(e)
+    
+    return True
+
+
+if __name__ == '__main__':
+    sys.exit(main())
