2217bf52322408d1b49bc7dad603878238c19173
Rem_ <black.white.red.are.the.best@gmail.com>
2022-03-13 01:16:18 +0900

    feat: Twitterリストの生成条件と生成処理を変更
    変更前：Twitterリストが存在しない場合に生成する
    変更後：Twitterリストが存在する場合に破棄し、生成する

--- src/twitter_app/logic/twitter_list_import.py
+++ src/twitter_app/logic/twitter_list_import.py
@@ -38,37 +38,41 @@ def do_logic(
         else:
             # TwitterAPIの実行
             for twitter_list_csv_file_path in twitter_list_csv_file_paths:
-                # Twitterリストが存在しない場合
+                # Twitterリスト名の生成
                 twitter_list_name: str = \
                     os.path.splitext(os.path.basename(twitter_list_csv_file_path))[0]
-                if twitter_users_util.has_twitter_list(api, twitter_list_name) == False:
-                    # Twitterリストの生成
-                    twitter_list = twitter_users_util.generate_twitter_list(api, twitter_list_name)
-                    
-                    # Twitterリストデータフレームの取得(TwitterリストCSVファイルの読み込み)
-                    twitter_list_df: pd.DataFrame = \
-                        pandas_util.read_twitter_list_file(twitter_list_csv_file_path)
-                    
-                    # ユーザの追加
-                    pyl.log_inf(lg, f'時間がかかるため気長にお待ちください。')
-                    for _, twitter_list_row in twitter_list_df.iterrows():
-                        if len(twitter_list_row) >= 2:
-                            twitter_users_util.add_user_to_twitter_list(
-                                    api,
-                                    twitter_list,
-                                    str(twitter_list_row[const_util.TWITTER_LIST_FILE_HEADER[0]]),
-                                    str(twitter_list_row[const_util.TWITTER_LIST_FILE_HEADER[1]])
-                                )
-                    
-                    # Twitterリストの破棄(ユーザが0人の場合)
-                    if len(twitter_list_df) == 0:
-                        twitter_users_util.destroy_twitter_list(api, twitter_list)
+                
+                # Twitterリストが存在する場合
+                if twitter_users_util.has_twitter_list(api, twitter_list_name) == True:
+                    twitter_users_util.destroy_twitter_list(api, twitter_list_name)
+                
+                # Twitterリストの生成
+                twitter_list = twitter_users_util.generate_twitter_list(api, twitter_list_name)
+                
+                # Twitterリストデータフレームの取得(TwitterリストCSVファイルの読み込み)
+                twitter_list_df: pd.DataFrame = \
+                    pandas_util.read_twitter_list_file(twitter_list_csv_file_path)
+                
+                # ユーザの追加
+                pyl.log_inf(lg, f'時間がかかるため気長にお待ちください。')
+                for _, twitter_list_row in twitter_list_df.iterrows():
+                    if len(twitter_list_row) >= 2:
+                        twitter_users_util.add_user_to_twitter_list(
+                                api,
+                                twitter_list,
+                                str(twitter_list_row[const_util.TWITTER_LIST_FILE_HEADER[0]]),
+                                str(twitter_list_row[const_util.TWITTER_LIST_FILE_HEADER[1]])
+                            )
+                
+                # Twitterリストの破棄(ユーザが0人の場合)
+                if len(twitter_list_df) == 0:
+                    twitter_users_util.destroy_twitter_list(api, twitter_list_name)
         
         pyl.log_inf(lg, f'Twitterリストインポートを終了します。')
     except Exception as e:
         # Twitterリストの破棄
         if twitter_list is not None:
-            twitter_users_util.destroy_twitter_list(api, twitter_list)
+            twitter_users_util.destroy_twitter_list(api, twitter_list.name)
         
         raise(e)
     
