0d1ec5a755056ca5abd2cb105d2fe48e75d1e4ff
Rem_ <black.white.red.are.the.best@gmail.com>
2022-04-19 00:47:28 +0900

    feat: Twitterリストインポート機能に引数を追加
    下記コマンドライン引数を追加
      - 差分ユーザ追加

--- src/twitter_app/logic/twitter_list_import.py
+++ src/twitter_app/logic/twitter_list_import.py
@@ -6,6 +6,7 @@ from typing import Any, Optional
 import pandas as pd
 import python_lib_for_me as pyl
 import tweepy
+from tweepy.models import ResultSet
 
 from twitter_app.util import const_util, pandas_util
 from twitter_app.util.twitter_api_v1_1.standard import twitter_users_util
@@ -14,7 +15,8 @@ from twitter_app.util.twitter_api_v1_1.standard import twitter_users_util
 def do_logic(
         api: tweepy.API,
         list_member_file_path_with_wildcard: str,
-        header_line_num: int
+        header_line_num: int,
+        add_only_users_with_diff: bool
     ) -> None:
     
     '''ロジック実行'''
@@ -40,11 +42,20 @@ def do_logic(
                 # リスト名の生成
                 list_name: str = os.path.splitext(os.path.basename(list_member_file_path))[0]
                 
-                # リストの破棄
-                twitter_users_util.destroy_list(api, list_name)
+                # 既存リストの取得
+                if add_only_users_with_diff == True:
+                    existing_lists: ResultSet = twitter_users_util.get_lists(api)
+                    for existing_list in existing_lists:
+                        if existing_list.name == list_name:
+                            list_ = existing_list
+                            break
                 
                 # リストの生成
-                list_ = twitter_users_util.generate_list(api, list_name)
+                if add_only_users_with_diff == True and list_ is None:
+                    list_ = twitter_users_util.generate_list(api, list_name)
+                elif add_only_users_with_diff == False:
+                    twitter_users_util.destroy_list(api, list_name)
+                    list_ = twitter_users_util.generate_list(api, list_name)
                 
                 # リストメンバーデータフレームの取得(リストメンバーファイルの読み込み)
                 list_member_df: pd.DataFrame = \
@@ -57,7 +68,8 @@ def do_logic(
                         [str(list_member[const_util.LIST_MEMBER_HEADER[0]]).strip()
                             for _, list_member in list_member_df.iterrows()],
                         [str(list_member[const_util.LIST_MEMBER_HEADER[1]])
-                            for _, list_member in list_member_df.iterrows()]
+                            for _, list_member in list_member_df.iterrows()],
+                        add_only_users_with_diff
                     )
         
         pyl.log_inf(lg, f'Twitterリストインポートを終了します。')
